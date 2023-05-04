4f4b27e1a8f40adc24c2d0fae8935d13723f527d
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-05-04 16:10:29 +0200

    🔧 Endre navn på jar som blir produsert

--- spoogle-backend/build.gradle.kts
+++ spoogle-backend/build.gradle.kts
@@ -1,3 +1,5 @@
+private val mainClass = "no.nav.helse.spoogle.AppKt"
+
 private val rapidsAndRiversVersion = "2023050308441683096263.f5a276d7bd28"
 private val junitVersion = "5.9.0"
 
@@ -15,5 +17,32 @@ dependencies {
 tasks {
     test {
         useJUnitPlatform()
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    jar {
+        archiveBaseName.set("app")
+
+        manifest {
+            attributes["Main-Class"] = mainClass
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
+        }
     }
 }
