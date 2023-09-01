d77a5918ff21c4fd0ab52984b03488f6b2d2f470
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-09-01 11:48:43 +0200

    🐛 Opprydding-dev må benytte seg av "sti", ikke "edge" i db

--- spoogle-opprydding-dev/src/test/resources/db/testperson/R__testperson.sql
+++ spoogle-opprydding-dev/src/test/resources/db/testperson/R__testperson.sql
@@ -1,7 +1,7 @@
 INSERT INTO node(id, id_type) VALUES ('${fødselsnummer}', 'FØDSELSNUMMER');
 INSERT INTO node(id, id_type) VALUES ('${organisasjonsnummer}', 'ORGANISASJONSNUMMER');
 
-INSERT INTO edge(node_a, node_b, ugyldig)
+INSERT INTO sti(forelder, barn, ugyldig)
 VALUES (
         (SELECT node_id FROM node WHERE id = '${fødselsnummer}'),
         (SELECT node_id FROM node WHERE id = '${organisasjonsnummer}'),
