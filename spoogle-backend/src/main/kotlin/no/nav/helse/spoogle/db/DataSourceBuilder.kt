a4b929f4fddbd35c59af8210f53c4b585147d52a
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2024-04-30 17:34:37 +0200

    🔧 Bruk ny autogenerert jdbc-url fra envvars
    En ny måte å koble til postgres-instanser gjelder for nye instanser etter
    18. april 2024. For å koble på disse er det enklere å bruke autogenerert
    jdbc-url enn å eksplisitt sette parametre selv.

--- spoogle-backend/src/main/kotlin/no/nav/helse/spoogle/db/DataSourceBuilder.kt
+++ spoogle-backend/src/main/kotlin/no/nav/helse/spoogle/db/DataSourceBuilder.kt
@@ -11,38 +11,31 @@ import java.time.Duration
 import javax.sql.DataSource
 
 internal class DataSourceBuilder(env: Map<String, String>) {
-    private val databaseHost: String = requireNotNull(env["DATABASE_HOST"]) { "host må settes" }
-    private val databasePort: String = requireNotNull(env["DATABASE_PORT"]) { "port må settes" }
-    private val databaseName: String = requireNotNull(env["DATABASE_DATABASE"]) { "databasenavn må settes" }
-    private val databaseUsername: String = requireNotNull(env["DATABASE_USERNAME"]) { "brukernavn må settes" }
-    private val databasePassword: String = requireNotNull(env["DATABASE_PASSWORD"]) { "passord må settes" }
+    private val dbUrl = requireNotNull(env["NAIS_DATABASE_SPOOGLE_SPOOGLE_JDBC_URL"]) { "JDBC url må være satt" }
 
-    private val dbUrl = String.format("jdbc:postgresql://%s:%s/%s", databaseHost, databasePort, databaseName)
+    private val hikariConfig =
+        HikariConfig().apply {
+            jdbcUrl = dbUrl
+            idleTimeout = Duration.ofMinutes(1).toMillis()
+            maxLifetime = idleTimeout * 5
+            initializationFailTimeout = Duration.ofMinutes(1).toMillis()
+            connectionTimeout = Duration.ofSeconds(30).toMillis()
+            maximumPoolSize = 1
+            metricRegistry =
+                PrometheusMeterRegistry(
+                    PrometheusConfig.DEFAULT,
+                    CollectorRegistry.defaultRegistry,
+                    Clock.SYSTEM,
+                )
+        }
 
-    private val hikariConfig = HikariConfig().apply {
-        jdbcUrl = dbUrl
-        username = databaseUsername
-        password = databasePassword
-        idleTimeout = Duration.ofMinutes(1).toMillis()
-        maxLifetime = idleTimeout * 5
-        initializationFailTimeout = Duration.ofMinutes(1).toMillis()
-        connectionTimeout = Duration.ofSeconds(30).toMillis()
-        maximumPoolSize = 1
-        metricRegistry = PrometheusMeterRegistry(
-            PrometheusConfig.DEFAULT,
-            CollectorRegistry.defaultRegistry,
-            Clock.SYSTEM
-        )
-    }
-
-    private val hikariMigrationConfig = HikariConfig().apply {
-        jdbcUrl = dbUrl
-        username = databaseUsername
-        password = databasePassword
-        initializationFailTimeout = Duration.ofMinutes(1).toMillis()
-        connectionTimeout = Duration.ofMinutes(1).toMillis()
-        maximumPoolSize = 2
-    }
+    private val hikariMigrationConfig =
+        HikariConfig().apply {
+            jdbcUrl = dbUrl
+            initializationFailTimeout = Duration.ofMinutes(1).toMillis()
+            connectionTimeout = Duration.ofMinutes(1).toMillis()
+            maximumPoolSize = 2
+        }
 
     private fun runMigration(dataSource: DataSource) =
         Flyway.configure()
