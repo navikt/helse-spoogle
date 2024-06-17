a12ce4965291661ad04690dedbd56c6b09585aca
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-06-17 11:33:29 +0200

    🗃️ Sett max pool size til 10
    Fremprovosert av varsel om at idleTimeout ikke har noen effekt med en fixed size pool :p
    
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- spoogle-backend/src/main/kotlin/no/nav/helse/spoogle/db/DataSourceBuilder.kt
+++ spoogle-backend/src/main/kotlin/no/nav/helse/spoogle/db/DataSourceBuilder.kt
@@ -20,7 +20,7 @@ internal class DataSourceBuilder(env: Map<String, String>) {
             maxLifetime = idleTimeout * 5
             initializationFailTimeout = Duration.ofMinutes(1).toMillis()
             connectionTimeout = Duration.ofSeconds(30).toMillis()
-            maximumPoolSize = 1
+            maximumPoolSize = 10
             metricRegistry =
                 PrometheusMeterRegistry(
                     PrometheusConfig.DEFAULT,
