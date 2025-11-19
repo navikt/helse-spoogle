8e945b247395ce835801b36d498dcc9c26931c05
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-11-19 20:34:24 +0100

    🩹 Oppdaterer konfigurasjon for eslint til fungerende tilstand
    Kjørte migreringsskriptet deres og fiksa ymse konfig-ting.

---
+++ spoogle-frontend/eslint.config.mjs
@@ -0,0 +1,70 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import typescriptParser from "@typescript-eslint/parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([
+    globalIgnores(["**/node_modules/", "**/.prettierrc", "**/.eslintrc.json"]),
+    {
+        extends: fixupConfigRules(compat.extends(
+            "eslint:recommended",
+            "plugin:react/recommended",
+            "plugin:import/recommended",
+            "plugin:jsx-a11y/recommended",
+            "plugin:@typescript-eslint/recommended",
+            "eslint-config-prettier",
+        )),
+
+        plugins: {
+            react: fixupPluginRules(react),
+            "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+
+            parser: typescriptParser,
+            ecmaVersion: 12,
+            sourceType: "module",
+
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+        },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+
+            "import/resolver": {
+                typescript: {
+                    alwaysTryTypes: true,
+                    project: "./tsconfig.json",
+                },
+            },
+        },
+
+        rules: {
+            "react/prop-types": "off",
+            "jsx-a11y/no-autofocus": "off",
+        },
+    },
+]);
